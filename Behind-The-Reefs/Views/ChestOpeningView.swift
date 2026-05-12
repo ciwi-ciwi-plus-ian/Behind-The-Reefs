e744ac9f37668cb4e9798badc97f79a818152439
BryanSamuelIan <146687755+BryanSamuelIan@users.noreply.github.com>
2026-05-12 22:48:45 +0700

    feat: add keys

--- Behind-The-Reefs/Views/ChestOpeningView.swift
+++ Behind-The-Reefs/Views/ChestOpeningView.swift
@@ -12,12 +12,28 @@ struct ChestOpeningView: View {
     @State private var chestOpened = false
     
     var body: some View {
-        ZStack {
+        VStack {
             // Chest
             Image(chestOpened ? "Behind_the_Reefs_TreasureChest-02" : "Behind_the_Reefs_TreasureChest-01")
                 .resizable()
                 .scaledToFit()
-                .frame(width: 220)
+                .frame(width: 400)
+            
+            // Keys
+            HStack(spacing: 14) {
+                
+                ForEach(0..<5, id: \.self) { index in
+                    
+                    Image("Behind_the_Reefs_Keys-0\(index + 1)")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 75, height: 75)
+                    
+                    
+                }
+            }.padding(.bottom,30)
+            
+            
         }
     }
     
