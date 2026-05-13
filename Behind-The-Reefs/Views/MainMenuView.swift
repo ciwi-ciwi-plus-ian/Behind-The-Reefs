aee0ccadb37a19bddb5b11023f4a4e4d6531ab7d
Ivanagrasielda <ivanagrasielda@gmail.com>
2026-05-13 13:33:57 +0700

    feat: create main menu screen

--- Behind-The-Reefs/Views/MainMenuView.swift
+++ Behind-The-Reefs/Views/MainMenuView.swift
@@ -8,13 +8,71 @@
 import SwiftUI
 
 struct MainMenuView: View {
+    
     var body: some View {
-        Text("Behind The Reefs")
-            .font(.custom("chewy", size: 28))
+        
+        NavigationStack {
+            
+            ZStack {
+                
+                // Background
+                Image("mainMenuBackground")
+                    .resizable()
+                    .scaledToFill()
+                    .ignoresSafeArea()
+                
+                VStack(spacing: 20) {
+                    
+                    Spacer()
+                    
+                    // Title
+                    Text("Behind The Reefs")
+                        .font(.largeTitle)
+                        .fontWeight(.bold)
+                    
+                    Spacer()
+                    
+                    // Continue Button
+//                    NavigationLink(destination: ContinueView()) {
+                        Text("Continue")
+                            .font(.title2)
+                            .fontWeight(.semibold)
+                            .foregroundColor(.white)
+                            .frame(width: 220, height: 60)
+                            .background(Color.blue)
+                            .cornerRadius(30)
+//                    }
+                    
+                    // New Game Button
+//                    NavigationLink(destination: NewGameView()) {
+                        Text("New Game")
+                            .font(.title2)
+                            .fontWeight(.semibold)
+                            .foregroundColor(.white)
+                            .frame(width: 220, height: 60)
+                            .background(Color.blue)
+                            .cornerRadius(30)
+//                    }
+                    
+                    // Credits Button
+//                    NavigationLink(destination: CreditsView()) {
+                        Text("Credits")
+                            .font(.title2)
+                            .fontWeight(.semibold)
+                            .foregroundColor(.white)
+                            .frame(width: 180, height: 50)
+                            .background(Color.gray)
+                            .cornerRadius(25)
+//                    }
+                    
+                    Spacer()
+                }
+                .padding()
+            }
+        }
     }
 }
 
-
-#Preview(traits: .landscapeRight) {
+#Preview(traits:.landscapeRight) {
     MainMenuView()
 }
