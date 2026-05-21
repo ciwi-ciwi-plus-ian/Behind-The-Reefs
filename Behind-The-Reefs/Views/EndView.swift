cb53a946d76971f7882bda0dc057521821652866
BryanSamuelIan <146687755+BryanSamuelIan@users.noreply.github.com>
2026-05-21 14:42:04 +0700

    feat: add bubble animation on endview

--- Behind-The-Reefs/Views/EndView.swift
+++ Behind-The-Reefs/Views/EndView.swift
@@ -13,11 +13,25 @@ struct EndView: View {
     @Environment(NavigationRouter.self) private var router
     @State private var buttonSoundPlayer: AVAudioPlayer?
     @State private var bgmPlayer: AVAudioPlayer?
+    @State private var showBubbles = false
 
     private let frameWidth:       CGFloat = 500
     private let frameHeight:      CGFloat = 300
     private let homeButtonWidth:  CGFloat = 100
     private let homeButtonHeight: CGFloat = 60
+    
+    private let bubbleConfigs: [Bubble] = [
+        .init(id: 0, xOffset: -180, size: 0.40, delay: 0.0, duration: 9.0),
+        .init(id: 1, xOffset: -90, size: 0.55, delay: 0.7, duration: 8.0),
+        .init(id: 2, xOffset: -40, size: 0.70, delay: 0.4, duration: 10.0),
+        .init(id: 3, xOffset: 60, size: 0.50, delay: 1.1, duration: 7.2),
+        .init(id: 4, xOffset: 105, size: 0.65, delay: 0.6, duration: 8.5),
+        .init(id: 5, xOffset: 170, size: 0.45, delay: 1.3, duration: 7.8),
+        .init(id: 6, xOffset: -125, size: 0.65, delay: 2.6, duration: 8.5),
+        .init(id: 7, xOffset: 175, size: 0.45, delay: 2.3, duration: 7.8),
+        .init(id: 8, xOffset: -255, size: 0.65, delay: 1.6, duration: 8.2),
+        .init(id: 9, xOffset: 255, size: 0.45, delay: 1.3, duration: 8.8)
+    ]
 
     var body: some View {
         ZStack {
@@ -30,6 +44,26 @@ struct EndView: View {
                 .navigationBarBackButtonHidden(true)
 
             ZStack {
+                ForEach(bubbleConfigs) { config in
+                    Image("bubbleParticle")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(
+                            width: 90 * config.size,
+                            height: 90 * config.size
+                        )
+                        .opacity(showBubbles ? 1 : 0)
+                        .offset(
+                            x: config.xOffset,
+                            y: showBubbles ? -950 : 950
+                        )
+                        .animation(
+                            Animation.linear(duration: config.duration)
+                                .delay(config.delay)
+                                .repeatForever(autoreverses: false),
+                            value: showBubbles
+                        )
+                }
 
                 Image("congratsFrame")
                     .resizable()
@@ -75,6 +109,7 @@ struct EndView: View {
         .onAppear {
             playEndingSound()
             HapticService.notification(.success)
+            showBubbles = true
         }
         .onDisappear {
             bgmPlayer?.stop()
